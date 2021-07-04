e1c4e5b3957f3efbd48fc3b6ce8812a835fb5df0
FlexFocus <mustafamire@gmail.com>
2021-07-04 17:22:15 +0100

    added styling for P tags

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+var firstCard = 10
+var secondCard = 11
+var hasBlackJack = false
+var isAlive = true
+var message = " "
+
+var sum = firstCard + secondCard
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card"
+}
+else if (sum === 21) {
+
+    message = "Whohoo, you've got Blackjack ! "
+    hasBlackJack = true
+
+} else {
+
+    message = "You're out of the game my friend :S "
+    isAlive = false
+
+
+}
+
+console.log(message)
+
+// var age = 21
+
+// if (age < 21) {
+
+//     console.log("you can not enter the club")
+// }
+
+// else {
+//     console.log("welcome ")
+
+// }
